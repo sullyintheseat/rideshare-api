30fe1bd3d8a87d74103dfe6a26bfe7b6cf074fc3
sullyintheseat <msullivan@digitalseat.com>
2019-01-10 14:19:52 -0600

    changed endpoint to signup

--- controllers/root.js
+++ controllers/root.js
@@ -71,5 +71,5 @@ module.exports.controller = (app) => {
   app.get('/share', RootController.adConnect);
   app.get('/share/:driverId', RootController.adConnect);
   app.post('/signup', RootController.signUp);
-  app.get('/beta/:id', RootController.lookUp);
+  app.get('/signup/:id', RootController.lookUp);
 }
