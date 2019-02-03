6363c73e67838495b8ec93018cffa769649d6cb0
sullyintheseat <msullivan@digitalseat.com>
2019-02-03 12:26:10 -0600

    test

--- index.js
+++ index.js
@@ -29,9 +29,10 @@ db.once('open', function() {
 var xPolicy			    = function (req, res, next) {
 	res.header('Access-Control-Allow-Origin', '*');
 	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-	res.header("Access-Control-Allow-Credentials" ,"true");
+  res.header("Access-Control-Allow-Credentials" ,"true");
+  res.header("Access-Control-Allow-Origin" ,"true");
 	//res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Origin, Origin, X-Requested-With, Content-Type, Accept, token, X-CSRF-TOKEN, api-key, authorization, content-type");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, token, X-CSRF-TOKEN, api-key, authorization, content-type");
   	next();
 };
 app.use(xPolicy);
