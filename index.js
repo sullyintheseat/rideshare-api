4169e2c49ee6932afb0b7da8c04b1b7f332699ec
sullyintheseat <msullivan@digitalseat.com>
2019-02-03 12:48:51 -0600

    cors

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ db.once('open', function() {
 var xPolicy			    = function (req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, ContentType');
   res.header("Access-Control-Allow-Credentials" ,"true");
   //res.header("Access-Control-Allow-Origin", "*");
   //res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
