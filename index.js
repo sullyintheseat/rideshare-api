2dd2b893e73c716c8943538740ee1cbbbe8bfc56
sullyintheseat <msullivan@digitalseat.com>
2019-02-03 12:27:57 -0600

    allow fix

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ var xPolicy			    = function (req, res, next) {
 	res.header('Access-Control-Allow-Origin', '*');
 	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header("Access-Control-Allow-Credentials" ,"true");
-  res.header("Access-Control-Allow-Origin" ,"true");
+  res.header("Access-Control-Allow-Origin", "*");
 	//res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, token, X-CSRF-TOKEN, api-key, authorization, content-type");
   	next();
