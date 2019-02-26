61e1c9eca8f122c00612d50ec05685229bac7b5d
sullyintheseat <msullivan@digitalseat.com>
2019-02-26 11:11:08 -0600

    testing

--- controllers/scanning.js
+++ controllers/scanning.js
@@ -49,7 +49,7 @@ ScanningController = {
 
   scoringScan: async (req, res) => {
     console.log(req.ip);
-    res.status(200).send(req.ip + ' ' + ' hit ' +  req.ips );
+    res.status(200).send(req.ip + ' ' + ' hit ' +  req.ips + ' ' + req.get('x-real-ip'));
   }
 }
 
